32ee384725395ee3a0b1eb879401611a72a08b77
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-05 18:08:12 +0000

    .

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,8 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   // Variáveis primeira carta
-    char estado1;
-    char cod1[3];
+    char estado1 [3];
+    char cod1[4];
     char nmcid1[30];
     int pop1;
     float area1;
@@ -16,8 +16,8 @@ int main() {
     int ptturist1;
 
   //Variáveis segunda carta
-  char estado2;
-  char cod2 [3];
+  char estado2 [3];
+  char cod2 [4];
   char nmcid2 [30];
   int pop2;
   float area2;
@@ -25,35 +25,65 @@ int main() {
   int ptturist2;
 
   // Área para entrada de dados
-  // Dados primeira carta
-  printf ("Carta 1: \n");
-  printf ("Estado: %d \n", estado1);
-  printf ("Código: %s \n", cod1);
-  printf ("Nome da Cidade: %s \n", nmcid1);
-  printf ("População: %d \n", pop1);
-  printf ("Área: %2.f km2 \n", area1);
-  printf ("PIB: %2.f bilhões de reais \n", pib1);
-  printf ("Número de pontos turísticos: %d \n", ptturist1);
+  printf ("Estado da primeira carta: ");
+  scanf("%d", estado1);
 
-  // Dados segunda carta
-  printf ("\nCarta 2: \n");
-  printf ("Estado: %d \n", estado2);
-  printf ("Código: %s \n", cod2);
-  printf ("Nome da Cidade: %s \n", nmcid2);
-  printf ("População: %d \n", pop2);
-  printf ("Área: %2.f km2 \n", area2);
-  printf ("PIB: %2.f bilhões de reais \n", pib2);
-  printf ("Número de pontos turísticos: %d \n", ptturist2);
+  printf ("Estado da segunda carta: ");
+  scanf("%d", estado2);
 
-  // Área para exibição dos dados da cidade
-  scanf ("%s", &estado1);
-  scanf ("%c", &cod1);
-  scanf("%s", &nmcid1);
-  scanf ("%d", pop1);
-  scanf("%2.f", area1);
-  scanf ("%2.f", pib1);
-  scanf("%d", ptturist1);
+  printf ("Código da primeira carta: ");
+  scanf ("%s", cod1);
 
+  printf ("Código da segunda carta: ");
+  scanf ("%s", cod2); 
 
+  printf ("Nome cidade da primeira carta: ");
+  scanf("%s", nmcid1);
+
+  printf ("Nome cidade da segunda carta: ");
+  scanf("%s", nmcid2);
+
+  printf ("População primeira carta: ");
+  scanf ("%2.f",&pop1);
+
+  printf ("População segunda carta: ");
+  scanf ("%2.f", pop2);
+
+  printf ("Área primeira carta: ");
+  scanf("%2.f", &area1);
+
+  printf ("Área segunda carta: ");
+  scanf("%2.f", &area2);
+  
+  printf ("PIB primeira carta: ");
+  scanf ("%2.f", &pib1);
+
+  printf ("PIB segunda carta: ");
+  scanf ("%2.f", &pib2);
+
+  printf ("Pontos turísticos primeira carta: ");
+  scanf("%d", &ptturist1);
+
+  printf ("Pontos turísticos segunda carta: ");
+  scanf("%d", &ptturist2); 
+
+ // Área para exibição dos dados da cidade
+    printf("\n--- CARTA 1 ---\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", cod1);
+    printf("Cidade: %s\n", nmcid1);
+    printf("População: %d\n", pop1);
+    printf("Área: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos turísticos: %d\n", ptturist1);
+
+    printf("\n--- CARTA 2 ---\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", cod2);
+    printf("Cidade: %s\n", nmcid2);
+    printf("População: %d\n", pop2);
+    printf("Área: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos turísticos: %d\n", ptturist2);
 return 0;
 } 
