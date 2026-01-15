5fc96a6d844efe18fc02b2ed0234bcbadf33eb19
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-15 14:37:37 -0300

    Initialize card variables and remove input prompts

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,118 +2,86 @@
 
 int main() {
 
-    // ============================
-    //      VARIÁVEIS CARTA 1
-    // ============================
-    char estado1[3];
-    char cod1[4];
-    char nmcid1[30];
-    unsigned long int pop1;
-    float area1;
-    float pib1;
-    int ptturist1;
-
-    // ============================
-    //      VARIÁVEIS CARTA 2
-    // ============================
-    char estado2[3];
-    char cod2[4];
-    char nmcid2[30];
-    unsigned long int pop2;
-    float area2;
-    float pib2;
-    int ptturist2;
-
-    // ============================
-    //      ENTRADA DE DADOS
-    // ============================
-    printf("=== PRIMEIRA CARTA ===\n");
-    printf("Estado: ");
-    scanf("%s", estado1);
-
-    printf("Código: ");
-    scanf("%s", cod1);
-
-    printf("Nome da cidade: ");
-    scanf("%s", nmcid1);
-
-    printf("População: ");
-    scanf("%lu", &pop1);
-
-    printf("Área: ");
-    scanf("%f", &area1);
-
-    printf("PIB: ");
-    scanf("%f", &pib1);
-
-    printf("Pontos turísticos: ");
-    scanf("%d", &ptturist1);
-
-    printf("\n=== SEGUNDA CARTA ===\n");
-    printf("Estado: ");
-    scanf("%s", estado2);
-
-    printf("Código: ");
-    scanf("%s", cod2);
-
-    printf("Nome da cidade: ");
-    scanf("%s", nmcid2);
-
-    printf("População: ");
-    scanf("%lu", &pop2);
-
-    printf("Área: ");
-    scanf("%f", &area2);
-
-    printf("PIB: ");
-    scanf("%f", &pib2);
-
-    printf("Pontos turísticos: ");
-    scanf("%d", &ptturist2);
-
-    // ============================
-    //     NOVAS PROPRIEDADES
-    // ============================
-    float densidade1 = pop1 / area1;
-    float pibpc1 = (pib1 * 1000000000) / pop1;
-
-    float densidade2 = pop2 / area2;
-    float pibpc2 = (pib2 * 1000000000) / pop2;
-
-    // ============================
-    //      SUPER PODER
-    // ============================
-    float superPoder1 = (float)pop1 + area1 + pib1 + (float) ptturist1 + pibpc1 + (1 / densidade1);
-    float superPoder2 = (float)pop2 + area2 + pib2 + (float) ptturist2 + pibpc2 + (1 / densidade2);
-
-    // ============================
-    //      SAÍDA DE DADOS
-    // ============================
-    printf("\n\n===== DADOS CADASTRADOS =====\n");
-
-    printf("\n--- PRIMEIRA CARTA ---\n");
-    printf("Estado: %s\n", estado1);
-    printf("Código: %s\n", cod1);
-    printf("Cidade: %s\n", nmcid1);
-    printf("População: %lu\n", pop1);
-    printf("Área: %.2fkm2\n", area1);
-    printf("PIB: %.2fbilhões de reais\n", pib1);
-    printf("Pontos turísticos: %d\n", ptturist1);
-    printf("Densidade populacional: %.2fhab/km2\n", densidade1);
-    printf("PIB per Capita: R$ %.2f\n", pibpc1);
-    printf("Super Poder: %.2f\n", superPoder1);
-
-    printf("\n--- SEGUNDA CARTA ---\n");
-    printf("Estado: %s\n", estado2);
-    printf("Código: %s\n", cod2);
-    printf("Cidade: %s\n", nmcid2);
-    printf("População: %lu\n", pop2);
-    printf("Área: %.2fkm2\n", area2);
-    printf("PIB:R$ %.2fbilhões de reais\n", pib2);
-    printf("Pontos turísticos: %d\n", ptturist2);
-    printf("Densidade populacional: %.2fhab/km2\n", densidade2);
-    printf("PIB per Capita: R$ %.2f\n", pibpc2);
-    printf("Super Poder: %.2f\n", superPoder2);
+    // CARTA 1 - inicializadas no nível aventureiro da 3ª semana //
+        char estado1[3] = MG;
+        char cod1[4] = MG01;
+        char nmcid1[30] = UBERABA;
+        unsigned long int pop1 = 356781;
+        float area1 = 4523957;
+        float pib1 = 59943;
+        int ptturist1 = 18;
+        float densidade1 = pop1 / area1;
+        float pibpc1 = (pib1 * 1000000000) / pop1;
+        float superPoder1 = (float)pop1 + area1 + pib1 + (float) ptturist1 + pibpc1 + (1 / densidade1);
+    
+   // CARTA 2 - inicializadas no nível aventureiro da 3ª semana //
+        char estado2[3] = MG;
+        char cod2[4] = MG02;
+        char nmcid2[30] = IPATINGA;
+        unsigned long int pop2 = 227731;
+        float area2 = 164884;
+        float pib2 = 17609176;
+        int ptturist2 = 15;
+        float densidade2 = pop2 / area2;
+        float pibpc2 = (pib2 * 1000000000) / pop2;
+        float superPoder2 = (float)pop2 + area2 + pib2 + (float) ptturist2 + pibpc2 + (1 / densidade2);
+    
+    // ENTRADA DE DADOS - comentado a partir do nível aventureiro da 3ª semana visto que as variáveis foram inicializadas //
+    //printf("=== PRIMEIRA CARTA ===\n");
+    //printf("Estado: ");
+    //scanf("%s", estado1);
+    //printf("Código: ");
+    //scanf("%s", cod1);
+    //printf("Nome da cidade: ");
+    //scanf("%s", nmcid1);
+    //printf("População: ");
+    //scanf("%lu", &pop1);
+    //printf("Área: ");
+    //scanf("%f", &area1);
+    //printf("PIB: ");
+    //scanf("%f", &pib1);
+    //printf("Pontos turísticos: ");
+    //scanf("%d", &ptturist1);
+    //printf("\n=== SEGUNDA CARTA ===\n");
+    //printf("Estado: ");
+    //scanf("%s", estado2);
+    //printf("Código: ");
+    //scanf("%s", cod2);
+    //printf("Nome da cidade: ");
+    //scanf("%s", nmcid2);
+    //printf("População: ");
+    //scanf("%lu", &pop2);
+    //printf("Área: ");
+    //scanf("%f", &area2);
+    //printf("PIB: ");
+    //scanf("%f", &pib2);
+    //printf("Pontos turísticos: ");
+    //scanf("%d", &ptturist2);
+ 
+    // SAÍDA DE DADOS comentado a partir do nível aventureiro da 3ª semana visto que as variáveis foram inicializadas //
+    //printf("\n\n===== DADOS CADASTRADOS =====\n");
+    //printf("\n--- PRIMEIRA CARTA ---\n");
+    //printf("Estado: %s\n", estado1);
+    //printf("Código: %s\n", cod1);
+    //printf("Cidade: %s\n", nmcid1);
+    //printf("População: %lu\n", pop1);
+    //printf("Área: %.2fkm2\n", area1);
+    //printf("PIB: %.2fbilhões de reais\n", pib1);
+    //printf("Pontos turísticos: %d\n", ptturist1);
+    //printf("Densidade populacional: %.2fhab/km2\n", densidade1);
+    //printf("PIB per Capita: R$ %.2f\n", pibpc1);
+    //printf("Super Poder: %.2f\n", superPoder1);
+    //printf("\n--- SEGUNDA CARTA ---\n");
+    //printf("Estado: %s\n", estado2);
+    //printf("Código: %s\n", cod2);
+    //printf("Cidade: %s\n", nmcid2);
+    //printf("População: %lu\n", pop2);
+    //printf("Área: %.2fkm2\n", area2);
+    //printf("PIB:R$ %.2fbilhões de reais\n", pib2);
+    //printf("Pontos turísticos: %d\n", ptturist2);
+    //printf("Densidade populacional: %.2fhab/km2\n", densidade2);
+    //printf("PIB per Capita: R$ %.2f\n", pibpc2);
+    //printf("Super Poder: %.2f\n", superPoder2);
 
     // ============================
     //      COMPARAÇÃO CARTAS - semana2
@@ -136,9 +104,12 @@ int main() {
     //printf("PIB per Capita venceu (1)? %d\n", pibpc2 > pibpc1);
     //printf("Super Poder venceu (1)? %d\n", superPoder2 > superPoder1);
 
-    // ============================
+    // MENU INTERATIVO - nível aventureiro 3ª semana
+        printf("### JOGO SUPER TRUNFO ###\n\n");
+        printf("Escolha uma das opções abaixo\n");
+        
+    
     //      COMPARAÇÃO CARTAS - semana3
-    // ============================
         printf("RESULTADO DA RODADA\n");
         printf("Atributo: Super Poder\n");
 
